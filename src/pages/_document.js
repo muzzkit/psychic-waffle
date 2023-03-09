c4c7b0c7378e9dcc37514e0c92119c898c3eadd4
muzzkit <muzkit48@gmail.com>
2023-03-09 18:15:31 +0100

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        <script crossOrigin src="https://blockonomics.co/js/pay_widget.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+
+
     </Html>
   )
 }
